8804837fc62e8b48d4881b302d657ea338dfcda6
zhu-jun-xian <2402302699@qq.com>
2020-11-25 11:30:27 +0800

    Update user.vue

--- front_end/front_end/src/components/user.vue
+++ front_end/front_end/src/components/user.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <el-container>
       <el-header style="text-align: right; font-size: 12px">
-        <span id="user_name" style="font-size: 20px">{{ username }}</span>
+        <span id="user_name" style="font-size: 20px" >{{ username }}</span>
         <!-- <el-button @click="selectuser1">查询</el-button> -->
         <el-button @click="exit">退出</el-button>
       </el-header>
@@ -44,7 +44,7 @@
 </el-main>
 </el-container>
 </el-container>
-<el-dialog title="修改用户信息" :visible.sync="userVisible" width="35%" @open="updateuseropen">
+<el-dialog title="修改用户信息" :visible.sync="userVisible" width="35%" @open="updateuseropen" @close="updateuserclose">
     <span>
       <el-form ref="passwordform" :model="passwordform" label-width="100px">
         <el-form-item label="登陆ID" prop="id">
@@ -275,7 +275,26 @@
                         console.log("...err...", err);
                     });
             },
+            updateuserclose() {
+                let id = this.$route.query.id;
+                axios({
+                        method: "post",
+                        url: "/api/getUserById1",
+                        data: {
+                            id: id,
+                        },
+                    })
+                    .then((response) => {
+                        console.log('updateuserclose' + body.name)
+                        let body = response.data;
+                        this.username = body.name
 
+
+                    })
+                    .catch((err) => {
+                        console.log("...err...", err);
+                    });
+            },
             addstusubmitForm() {
                 axios({
                         method: "post",
@@ -344,9 +363,9 @@
                                 break
                         }
                         this.userVisible = false
-                        this.$router.push({
-                            path: "/",
-                        });
+                            // this.$router.push({
+                            //     path: "/",
+                            // });
                     })
                     .catch((err) => {
                         console.log("...err...", err);
@@ -459,7 +478,7 @@
                         .then((response) => {
                             let body = response.data;
                             switch (body) {
-                                case "删除成功":
+                                case "success":
                                     this.$message({
                                         type: 'success',
                                         message: '修改用户信息成功!',
