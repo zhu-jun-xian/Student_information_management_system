bcec367ceef3feb5f3a5a748f25b8df211b860ad
zhoumengjia <1589222075@qq.com>
2020-11-26 09:14:53 +0800

    zhoumengjia

--- front_end/front_end/.history/src/components/User/selectstudent_20201126091119.vue
+++ front_end/front_end/.history/src/components/User/selectstudent_20201126091119.vue
@@ -246,7 +246,7 @@ export default {
         url:"/api/SelectMessagesByStuAll",
         data:{
           stuName:this.selectfuzzyForm.fuzzyname,
-          stuID:this.selectfuzzyForm.fuzzynum,
+          stuNum:this.selectfuzzyForm.fuzzynum,
           stuClass:this.selectfuzzyForm.fuzzyclass,
           stuSex:this.selectfuzzyForm.fuzzysex,
           stuDep:this.selectfuzzyForm.fuzzyacademy,
@@ -257,7 +257,7 @@ export default {
         let body = response.data;
         this.stuData = [];
         this.stuData = body;
-        this.selectfuzzyVisible=false;
+        this.selec
         console.log(JSON.stringify(body));
       });
     },
