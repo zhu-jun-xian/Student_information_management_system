dc6449f18b6af9fe7641cbde688892c0199a2edc
youxuming910-chen <1109895644@qq.com>
2020-11-27 11:10:39 +0800

    Update user.vue

--- front_end/front_end/src/components/user.vue
+++ front_end/front_end/src/components/user.vue
@@ -319,7 +319,7 @@
             sumdialogopen() {
                 axios({
                         method: "get",
-                        url: "/api/CountStuInfo1",
+                        url: "/api/CountStuInfo",
                     })
                     .then((response) => {
                         let body = response.data;
